6c7727f048f58dbc40a6cad31ef017f091173781
ivan <ivan-guita@hotmail.com>
2023-03-18 13:25:16 -0300

    as quatro funções de crud da api ja existem e ja foram testadas

--- src/app.js
+++ src/app.js
@@ -5,8 +5,18 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
 const router = express.Router();
 
+/*covertendo o conteudo recebido pelo app em json*/
+app.use(bodyParser.json());
+
+/*confirando o tamanho da url que eu quero receber da requisição*/
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+/*rota para pegar dados*/
 const route = router.get('/',(req,res,next)=>{
 
     res.status(200).send({
@@ -15,6 +25,43 @@ const route = router.get('/',(req,res,next)=>{
     });
 });
 
+/*Rota para enviar dados*/
+const create = router.post('/',(req,res,next)=>{
+
+    res.status(201).send(req.body);
+
+});
+
+/*Rota para editar dados no caso eu tenho que passar o id pra ela saber qual eo dado que ela tem que editar
+  por isso diferente das outras ela tem um  /:id na url
+*/
+
+const put = router.put('/:id',(req,res,next)=>{
+     
+    const id = req.params.id;
+
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+
+});
+
+
+
+const del = router.delete('/:id',(req,res,next)=>{
+     
+    
+    res.status(200).send(req.body);
+})
+
+
+
+
+
 app.use('/',route);
+app.use('/products',create);
+app.use('/products',put);
+app.use('/products',del);
 
 module.exports = app;
