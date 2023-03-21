58933663df25f2277f102ab81abc24baf7d8b020
ivan <ivan-guita@hotmail.com>
2023-03-21 10:26:54 -0300

    foi adicionado um controller agora a responsabilidade de executar as requisições foi delegada pra ele o app so vai adimnistrar as rotas

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,48 +5,12 @@ const express = require('express');
 
 const router = express.Router();
 
+const controller = require('../controllers/product-controller');
 
-/*Rota para enviar dados*/
-const create = router.post('/',(req,res,next)=>{
+router.post('/', controller.post);
 
-    res.status(201).send(req.body);
+router.put('/:id',controller.put);
 
-});
-
-
-
-/*Rota pra pegar dados*/
-router.get('/',(req,res,next)=>{
-
-    res.status(200).send({
-        title:"Node store API",
-        version: "0.0.1"
-    });
-});
-
-/*Rota para editar dados no caso eu tenho que passar o id pra ela saber qual eo dado que ela tem que editar
-  por isso diferente das outras ela tem um  /:id na url
-*/
-
-router.put('/:id',(req,res,next)=>{
-     
-    const id = req.params.id;
-
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-
-});
-
-
-
-router.delete('/:id',(req,res,next)=>{
-     
-    
-    res.status(200).send(req.body);
-});
-
-
+router.delete('/:id',controller.delete);
 
 module.exports = router; 
